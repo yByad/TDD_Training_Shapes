0be556eeda691f1ac4cbf7e7fa157db829cfb42d
Youssef Byad <youssef.byad@gmail.com>
2016-02-16 17:32:12 +0100

    commit version 1.0

--- src/main/java/Shapes.java
+++ src/main/java/Shapes.java
@@ -1,5 +1,10 @@
+import java.io.StringWriter;
 
 public class Shapes {
+
+	public Shapes(StringWriter out) {
+		// TODO Auto-generated constructor stub
+	}
 
 	public void area(String string, String string2) {
 		// TODO Auto-generated method stub
